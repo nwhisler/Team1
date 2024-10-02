cadeac3e7a80d35b3669bd7e71cd5d7961f9d780
Nick Whisler <41985358+nwhisler@users.noreply.github.com>
2024-10-02 14:21:06 -0400

    Added client and server

---
+++ client.py
@@ -0,0 +1,73 @@
+import sys
+import socket
+import selectors
+import types
+import random
+
+sel = selectors.DefaultSelector()
+
+def start_connections(host, port):
+
+    connid = random.randrange(1, 50, 3)
+
+    server_addr = (host, port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex(server_addr)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    total_correct = 0
+    data = types.SimpleNamespace(
+        connid=connid,
+        recv_total=0,
+        message="",
+        complete = "",
+        outb=b"",
+    )
+    sel.register(sock, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.recv_total += len(recv_data)
+            if recv_data.decode() == "To start game type start":
+                print(recv_data.decode())
+                data.message = input("Input: ")
+            elif "Quiz completed" in recv_data.decode():
+                print(recv_data.decode())
+                sel.unregister(sock)
+                sock.close()
+            else:
+                print(recv_data.decode())
+                data.message = input("Answer: ")
+        if not recv_data:
+            if data.complete == "Quiz completed":
+                sel.unregister(sock)
+                sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and len(data.message) > 0:
+            data.outb = data.message.encode()
+        if data.outb:
+            sent = sock.send(data.outb)
+            data.message = ""
+            data.outb = b""
+
+
+host, port = sys.argv[1], int(sys.argv[2])
+
+start_connections(host, port)
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
---
+++ server.py
@@ -0,0 +1,74 @@
+import sys
+import socket
+import selectors
+import types
+import errno 
+
+sel = selectors.DefaultSelector()
+
+questions = {"Question 1": "Answer 1","Question 2": "Answer 2","Question 3": "Answer 3","Question 4": "Answer 4"}
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    conn.setblocking(False)
+    conn.send("To start game type start".encode())
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"", correct=0, questions=[], previousQuestion = "")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            if len(data.previousQuestion) > 0:
+                print(data.previousQuestion)
+                answer = recv_data.decode()
+                if answer == questions[data.previousQuestion]:
+                    data.correct += 1
+                data.outb = recv_data
+            else:
+                if(recv_data.decode() == "Start" or recv_data.decode() == "start"):
+                    data.outb += recv_data
+        else:
+            if(len(data.questions) == len(questions.keys())):
+                sel.unregister(sock)
+                sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            if(len(data.questions)) != len(questions.keys()):
+                for questionKey in questions.keys():
+                    if questionKey not in data.questions:
+                        sock.send(questionKey.encode())
+                        data.previousQuestion = questionKey
+                        data.questions.append(questionKey)
+                        data.outb = b""
+                        break
+            else:
+                sock.send(("Quiz completed number of correct " + str(data.correct)).encode())
+                sel.unregister(sock)
+                sock.close()
+
+host, port = sys.argv[1], int(sys.argv[2])
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+sockets = set()
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:            
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
